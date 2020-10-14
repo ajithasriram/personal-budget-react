f451d661c2ca7912d6e38d93859ef2c84bafc2bb
Aparajitha Sriram <33689863+ajithasriram@users.noreply.github.com>
2020-10-14 00:45:51 -0400

    Brought PBudget code to React

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero" id="title" role="banner" aria-roledescription="Page title">
+            <h1>Personal Budget</h1>
+            <h2>A personal-budget management app</h2>
+    </div>
+  );
+}
+
+export default Hero;
